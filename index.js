450abb70db56f28d049bf8bc0a02e7dc70924518
zekmanzek <mzybro@gmail.com>
2026-04-10 00:11:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-// MongoDB bağlantısı
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+// MongoDB bağlantısı (Render'da MONGODB_URI olarak tanımlı)
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(()=>console.log("✅ Premium DB Connected Successfully"))
   .catch(err=>console.error("DB Error:", err));
 
