e092c6732af8fef3ddd1e683307ccbd87ec543a2
zekmanzek <mzybro@gmail.com>
2026-04-29 00:12:30 +0300

    Update app.js

--- public/app.js
+++ public/app.js
@@ -5,7 +5,7 @@ const API = window.location.origin;
 let user, tasks = [], miningInterval; 
 let lbAllTime = [], lbDaily = []; 
 let currentLbTab = 'all'; 
-const AdController = window.Adsgram ? window.Adsgram.init({ blockId: "28938" }) : null; 
+const AdController = window.Adsgram ? window.Adsgram.init({ blockId: "27433" }) : null; 
 let tvWidgetCreated = false;
 
 function triggerHaptic(type = 'light') { 
